52ccdfa4a0a259d74bba980c5e6035366095a4cb
Eguzky <eguzky84@gmail.com>
2018-11-05 21:12:35 -0500

    Update Utilities/pokemonsearch.py
    Initial version of Pokemon search

---
+++ Utilities/pokemonsearch.py
@@ -0,0 +1,34 @@
+from CombatCode.pokemondex import BattlePokedex
+
+#This Will Be The Search Function To Find Pokemon By Type, Name, Number, Move (To Do), etc
+
+def pokenumsearch(pokenum):
+    try:
+        pokenum = int(pokenum)
+    except ValueError:
+        print('This Must Be A Valid Number')
+        return
+
+    results = pokesearch('num', pokenum)
+    print(results)
+
+def poketypesearch(poketype):
+    poketype = poketype.capitalize()
+    results = pokesearch('types', poketype)
+    print(results)
+
+def pokesearch(category, lookingFor):
+    #We Will Assume The Category & LookingFor Will Be Formatted Correctly
+    result = []
+    for key in BattlePokedex:
+        if isinstance(BattlePokedex[key][category], int) or isinstance(BattlePokedex[key][category], str):
+            val = BattlePokedex[key][category]
+            if val == lookingFor:
+                result.append(BattlePokedex[key]['species'])
+        if isinstance(BattlePokedex[key][category], dict) or isinstance(BattlePokedex[key][category], list):
+            for val in BattlePokedex[key][category]:
+                if val == lookingFor:
+                    result.append(BattlePokedex[key]['species'])
+
+    return result
+
