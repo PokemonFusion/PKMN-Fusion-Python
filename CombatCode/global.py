c23a17771e4a0773a84a08e49e5138c42f6f50ca
Koden02 <koden_kabau@yahoo.com>
2018-11-27 16:05:39 -0500

    Fixed some cases on moves and added the moves class

---
+++ CombatCode/global.py
@@ -0,0 +1,31 @@
+from moves import BattleMoveDex
+
+def keycheck(dictionary, subdictionary, key) -> object:
+    if key in dictionary[subdictionary]:
+        return dictionary[subdictionary][key]
+    else:
+        return None
+
+
+class Moves:
+
+    def __init__(self, move: str):
+        def movecheck(key):
+            return keycheck(BattleMoveDex, move, key)
+        
+        self.num = movecheck("num")
+        self.accuracy = movecheck("accuracy")
+        self.basePower = movecheck("basePower")
+        self.category = movecheck("category")
+        self.desc = movecheck("desc")
+        self.shortDesc = movecheck("shortDesc")
+        self.id = movecheck("id")
+        self.name = movecheck("name")
+        self.pp = movecheck("pp")
+        self.priority = movecheck("priority")
+        self.flags = movecheck("flags")
+        self.secondary = movecheck("secondary")
+        self.target = movecheck("target")
+        self.type = movecheck("type")
+        self.zMovePower = movecheck("zMovePower")
+        self.contestType = movecheck("contestType")
