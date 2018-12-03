0dd64804cde016c778ea446d8a8fa46dd998a162
Koden02 <koden_kabau@yahoo.com>
2018-12-03 04:17:45 -0500

    Added functionextraction helper code
    the helper code functionextraction will extract the dictionaries and make the new files needed in a nice neat package.  currently is working, for the most part... I think.

---
+++ HelperCode/functionextraction.py
@@ -0,0 +1,81 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('')))
+from CombatCode.moves import BattleMovedex
+from CombatCode.abilities import BattleAbilities
+from pprint import pprint
+
+basefolder = "HelperCode\\FunctionFiles"
+task = ""
+workingfolder = ""
+data = ""
+tododata = ""
+modulelist = []
+
+def makedir(dir):
+    try:
+        os.mkdir(dir)
+    except Exception:
+        pass
+
+def dicscan(dictionary : dict, masterkey : str = None):
+    """Search for function then replace the string with a function call using eval()"""
+    global data #this is global because of the recursion that happens
+    global workingfolder
+    global task
+    global tododata
+    global modulelist
+    for key, val in dictionary.items():
+        if masterkey == None:
+            data = ""
+        if isinstance(val,dict):
+            if masterkey == None:
+                dicscan(dictionary[key], key)
+            else:
+                dicscan(dictionary[key], masterkey)
+        elif isinstance(val, str):
+            if val.startswith("function ("):
+                if data != "":
+                    data += "\n\n"
+                functionname = str(masterkey + "." + key)
+                functionstring = val[0: val.find(')') + 1].replace("function", key)
+                data += "def " + functionstring + ':\n\t"""' + val + '\n\t""" \n\tpass'
+                dictionary[key] = "[deleteme]" + functionname + "[/deleteme]"
+                tododata += "  ☐ " + functionname + "\n"
+                modulelist.append(masterkey)
+        
+        if masterkey == None and data != "":
+            with open(os.path.join(workingfolder, key + ".py"),"w+") as funfile:
+                funfile.write(data)
+
+def writenewdict(dictionary, dicname, dicfile):
+    global basefolder
+    with open(os.path.join(basefolder,dicfile), "w+") as filename:
+        filename.write("from " + task + " import * \n\n")
+        filename.write(dicname + " = ")
+        pprint(dictionary, stream=filename)
+
+def writenewinit():
+    global modulelist, workingfolder
+    with open(os.path.join(workingfolder,"__init__.py"), "w+") as init:
+        init.write("__all__ = ")
+        pprint(sorted(list(set(modulelist))), stream= init)
+
+makedir(basefolder)
+with open(os.path.join(basefolder, "TODO"), "w+", encoding='utf-8') as todofile:
+    pass
+tasks = {"Abilities" : { "dictionary" : BattleAbilities, "file" : "abilities.py", "dicname" : "BattleAbilities"} , 
+         "Moves": {"dictionary" : BattleMovedex, "file" : "moves.py", "dicname" : "BattleMovedex"} 
+         }
+
+for task in tasks:
+    tododata = ""
+    modulelist = []
+    workingfolder = basefolder + "\\" + task
+    makedir(workingfolder)
+    dicscan(tasks[task]["dictionary"])
+    with open(os.path.join(basefolder, "TODO"), "a+", encoding='utf-8') as todofile:
+        todofile.write("\n" + task + ":\n")
+        todofile.write(tododata)
+    writenewdict(tasks[task]["dictionary"], tasks[task]["dicname"], tasks[task]["file"])
+    writenewinit()
+
